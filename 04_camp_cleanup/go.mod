10a563574d783a91afc81d506b24b555e242f37c
yottta <andreic9203@gmail.com>
2022-12-11 22:00:42 +0200

    solution(day4): all

---
+++ 04_camp_cleanup/go.mod
@@ -0,0 +1,3 @@
+module github.com/yottta/aoc2022/04_camp_cleanup
+
+go 1.19
---
+++ 04_camp_cleanup/part1/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Range struct {
+	start int
+	stop  int
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func parseRange(in string) Range {
+	split := strings.Split(in, "-")
+	if len(split) != 2 {
+		panic(fmt.Errorf("wrong input: %s", in))
+	}
+	start, err := strconv.Atoi(split[0])
+	must(err)
+	end, err := strconv.Atoi(split[1])
+	must(err)
+	return Range{
+		start: start,
+		stop:  end,
+	}
+}
+
+func fullyOverlap(r1 Range, r2 Range) bool {
+	if r1.start > r2.start {
+		return r1.stop <= r2.stop
+	}
+	if r2.start > r1.start {
+		return r2.stop <= r1.stop
+	}
+	return true
+}
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	reader := bufio.NewReader(file)
+	var (
+		end   bool
+		total int
+	)
+	for {
+		if end {
+			break
+		}
+		r, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("error reading string", err)
+			end = true
+		}
+		r = strings.ReplaceAll(r, "\n", "")
+		if len(r) == 0 {
+			break
+		}
+		split := strings.Split(r, ",")
+		if len(split) != 2 {
+			panic(fmt.Errorf("wrong input %s", r))
+		}
+
+		r1 := parseRange(split[0])
+		r2 := parseRange(split[1])
+
+		if fullyOverlap(r1, r2) {
+			total++
+		}
+	}
+	fmt.Println(total)
+}
