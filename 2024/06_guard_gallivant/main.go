602a9f58fcb0d0588f49e385af4cea2c29035e76
yottta <andreic9203@gmail.com>
2024-12-17 19:39:40 +0200

    aoc(2024): day 6 p2

--- 2024/06_guard_gallivant/main.go
+++ 2024/06_guard_gallivant/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 
 	aoc2024 "github.com/yottta/advent-of-code/2024"
 )
@@ -12,26 +11,42 @@ func main() {
 }
 
 func part1(content []string) {
+	//return
 	_map, sold := parseContent(content)
-	res := walk(_map, sold)
+	res, _ := walk(_map, sold)
 	//printMap(_map, sold)
 	fmt.Println(len(res))
 
 }
+
+// 1656 - too high
 func part2(content []string) {
 	_map, sold := parseContent(content)
-	printMap(_map, sold, false)
-	res := walk(_map, sold)
-	printMap(_map, sold, true)
-	fmt.Println(len(res))
+	soldBak := *sold
+	res, _ := walk(_map, sold)
+	sold = &soldBak
+	var validObstacles int
+	for pos := range res {
+		testSold := soldBak
+		if pos.x == soldBak.currentPosition.x && pos.y == soldBak.currentPosition.y {
+			continue
+		}
+		_map[pos.y][pos.x].obstacle = true
+		if _, ok := walk(_map, &testSold); !ok {
+			validObstacles++
+		}
+		_map[pos.y][pos.x].obstacle = false
+
+	}
+	fmt.Println(validObstacles)
 }
 
-func walk(_map [][]position, sold *soldier) map[position]struct{} {
+func walk(_map [][]position, sold *soldier) (map[position]struct{}, bool) {
 	var (
-		out = map[position]struct{}{}
+		out    = map[position]struct{}{}
+		passed = map[string]struct{}{}
 	)
 
-	_map[sold.currentPosition.y][sold.currentPosition.x].passes++
 	for {
 		nextSoldPos := sold.nextPosition()
 		// got out of the map to the left or right
@@ -42,16 +57,20 @@ func walk(_map [][]position, sold *soldier) map[position]struct{} {
 		if nextSoldPos.y < 0 || nextSoldPos.y >= len(_map[0]) {
 			break
 		}
+		passedKey := fmt.Sprintf("%d#%d#%d#%d", sold.currentPosition.x, sold.currentPosition.y, sold.deltaX, sold.deltaY)
+		if _, ok := passed[passedKey]; ok {
+			return nil, false
+		}
 		// if it's an obstacle ahead, rotate and reiterate
 		if _map[nextSoldPos.y][nextSoldPos.x].obstacle {
 			sold.rotate()
 			continue
 		}
-		_map[nextSoldPos.y][nextSoldPos.x].passes++
 		sold.currentPosition = nextSoldPos
-		out[nextSoldPos] = struct{}{} // register the place where the soldier was
+		passed[passedKey] = struct{}{} // register the position and the direction the soldier is passing the slot
+		out[nextSoldPos] = struct{}{}  // register the place where the soldier was
 	}
-	return out
+	return out, true
 }
 
 func parseContent(content []string) ([][]position, *soldier) {
@@ -83,7 +102,7 @@ func parseContent(content []string) ([][]position, *soldier) {
 	return _map, sold
 }
 
-func printMap(_map [][]position, sold *soldier, passes bool) {
+func printPath(_map [][]position, steps map[position]struct{}, sold soldier) {
 	for x := 0; x < len(_map); x++ {
 		fmt.Println()
 		for y := 0; y < len(_map[x]); y++ {
@@ -91,11 +110,12 @@ func printMap(_map [][]position, sold *soldier, passes bool) {
 			val := "."
 			if p.obstacle {
 				val = "#"
-			} else if passes {
-				val = strconv.Itoa(p.passes)
+			}
+			if _, ok := steps[p]; ok {
+				val = "x"
 			}
 			if p == sold.currentPosition {
-				val = "S"
+				val = "^"
 			}
 			fmt.Printf("%s", val)
 		}
@@ -106,7 +126,6 @@ func printMap(_map [][]position, sold *soldier, passes bool) {
 type position struct {
 	x, y     int
 	obstacle bool
-	passes   int
 }
 
 type soldier struct {
